8545f674213d2a49584c72dec46410e27d57d266
socceryto <44013892+socceryto@users.noreply.github.com>
2018-10-10 14:36:27 +0900

    Add files via upload

---
+++ J1_1.java
@@ -0,0 +1,10 @@
+public class J1_1{
+    public static void main (String[] args) {
+        int x=25;
+        int y=40;
+        
+        System.out.println("xの値は" + x + "です。");
+        System.out.println("yの値は" + y + "です。");
+        System.out.println("x+yの値は" + x+y + "です。");
+    }
+}
